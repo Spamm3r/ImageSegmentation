65fa903264bf516c1b91e6d3c5374f86e64aae50
Spamm3r <tspamm3r@gmail.com>
2019-11-04 15:28:33 +0100

    PCA

--- utilities.py
+++ utilities.py
@@ -1,15 +1,25 @@
 from scipy.io import loadmat
 import matplotlib.pyplot as plt
 import numpy as np
+from numpy import mean
 from sklearn import cluster, mixture
-import pandas as pd
-from sklearn.decomposition import PCA
+
 
 class Utilities:
     def __init__(self, n_classes):
         self.n_classes = n_classes
         self.data, self.gt, self.data_a = self.mat_to_array()  # (83, 86, 204) ;  (83, 86)
         # kmeans = self.kmeans_for_each_pixel(self.data, self.n_classes)
+        kmeans = self.kmeans_custom(self.data, self.n_classes, 1)
+        self.pca(kmeans[0])
+
+    def kmeans_custom(self, data, n_classes, columns):
+        x, y, z = data.shape
+        clustered = np.zeros((x, y, z))
+        for i in range(columns):
+            for j in range(y):
+                clustered[i][j] = cluster.KMeans(n_clusters=n_classes).fit(self.data[i][j].reshape(-1, 1)).labels_
+        return clustered
 
     def kmeans_for_each_pixel(self, data, n_classes):
         x, y, z = data.shape
@@ -35,3 +45,14 @@ class Utilities:
         #     data_a[:, :, i] = (data_a[:, :, i] - np.mean(data_a[:, :, i])) / np.std(data_a[:, :, i])
 
         return data, gt, data_a
+
+
+    def pca(self, data):
+        m = mean(data)
+        c = data - m
+        v = np.cov(c.T)
+        values, vectors = np.linalg.eig(v)
+        p = vectors.T.dot(c.T)
+        # print(p.T)
+        plt.imshow(np.real(p.T))
+        plt.show()
